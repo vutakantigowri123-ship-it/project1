a2a3a53763d72ecb617ded7242b4908e9a1a34f2
Gowri <vutakantigowri123@gmail.com>
2026-06-02 09:54:26 +0530

    new file

---
+++ python4.py
@@ -0,0 +1,12 @@
+#new project
+# Simple Python program
+
+# Print a greeting
+print("Hello, welcome to Python!")
+
+# Do a quick calculation
+a = 5
+b = 3
+sum_result = a + b
+
+print("The sum of", a, "and", b, "is:", sum_result)
